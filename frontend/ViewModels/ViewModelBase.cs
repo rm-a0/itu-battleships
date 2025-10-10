b2b065a2f245be70a513a41831667009e2000e6a
rm-a0 <michalrepcik9@gmail.com>
2025-10-10 16:50:46 +0200

    feat: add basic avalonia project structure, install all dependencies and expand gitignore

---
+++ frontend/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace BattleshipsAvalonia.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ frontend/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace BattleshipsAvalonia.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
